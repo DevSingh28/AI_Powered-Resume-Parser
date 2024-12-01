5c89d951e658c3f3d4494d59a1e61ef4d8ed5ae3
Dev Singh <devsingh2017dp@gmail.com>
2024-12-01 03:17:21 -0600

    final commit

--- resume_parser/settings.py
+++ resume_parser/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not os.path.exists(MEDIA_ROOT):
+    os.makedirs(MEDIA_ROOT)
 
 
 
